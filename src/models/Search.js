23812349f71fb33e7745ec671e96a56370a25266
Alex Garcia <alex.g9415@gmail.com>
2020-07-15 22:43:40 -0700

    building search query

---
+++ src/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {api_key} from '../config';
+
+//https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&APPID=${API_KEY};
+
+export default class Search {
+    constructor(city){
+        this.city = city;
+    }
+
+    async searchCity(){
+        try{
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=imperial&APPID=${api_key}`);
+        } catch(error){
+            console.log(`We ran into an error: ${error}`);
+        }
+    }
+}
